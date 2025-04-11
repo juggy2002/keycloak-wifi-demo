13c00b03322124f49b680c804bf2ce10459276c9
juggy2002 <singhjugaad@icloud.com>
2025-04-11 18:08:40 +0100

    k

--- src/keycloak.js
+++ src/keycloak.js
@@ -2,7 +2,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080/',
+  url: 'http://127.0.0.1:4040/',
   realm: 'myrealm',
   clientId: 'my-client',
 });
